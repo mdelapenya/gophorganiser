019fff5ffd62bfd1e3d15783c80411faa8e5cb72
Manuel de la Peña <mdelapenya@gmail.com>
2019-12-22 20:02:49 +0100

    Prompt user for directories to process
    We are going to ask the user for another directory, until no more directories are added

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mdelapenya/gophorganiser
+
+go 1.13
+
+require (
+	github.com/manifoldco/promptui v0.6.0
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Print("Hello Gophorganiser!")
+
+	picturesDirs := []string{}
+
+	// add source directories
+	for true {
+		picturesDir := promptPath()
+		picturesDirs = append(picturesDirs, picturesDir)
+
+		if !promptContinue() {
+			break
+		}
+	}
+
+	log.Printf("We are going to process the following directories: %v", picturesDirs)
+}
+
+func pathExists(p string) error {
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return errors.New("Path does not exist")
+	}
+
+	return nil
+}
+
+func promptContinue() bool {
+	validate := func(input string) error {
+		validInputs := []string{
+			"y", "yes", "n", "no", "",
+		}
+		for _, b := range validInputs {
+			if b == strings.ToLower(input) {
+				return nil
+			}
+		}
+		return errors.New("Accepted values: y, yes, no, n")
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Do you want to add more directories? (y/N)",
+		Default:  "N",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	lower := strings.ToLower(result)
+	if "y" == lower || "yes" == lower {
+		return true
+	}
+
+	return false
+}
+
+func promptPath() string {
+	prompt := promptui.Prompt{
+		Label:    "Location of the pictures: ",
+		Default:  "N",
+		Validate: pathExists,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	return result
+}
